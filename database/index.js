7bbe299a0d8e7dcf6a5b8d86d76d194868d90540
Therese Brown <browntm96@gmail.com>
2019-04-05 07:50:14 -0700

    Fetch images and main images from the database

--- database/index.js
+++ database/index.js
@@ -3,4 +3,27 @@ const mysqlConfig = require('./config.js');
 
 const connection = mysql.createConnection(mysqlConfig);
 
-module.exports = connection;
+const getAllImages = function(callback) {
+  connection.query('SELECT * FROM products', (err, results) => {
+    if (err) {
+      callback(err, null);
+    }
+    callback(null, results);
+  });
+};
+
+const getAllMains = function(callback) {
+  connection.query('SELECT main_img FROM products', (err, results) => {
+    if (err) {
+      console.log('The problem is in the database')
+      callback(err, null);
+    }
+    callback(null, results);
+  });
+};
+
+module.exports = {
+  connection,
+  getAllImages,
+  getAllMains
+};
