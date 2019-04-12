67af4614f363ff3b4943dc52a45ba2fa699a479e
Therese Brown <browntm96@gmail.com>
2019-04-12 00:23:23 -0700

    Render the product images to the dom

--- client/src/Components/ProductView.jsx
+++ client/src/Components/ProductView.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styles from './Styles.module.css';
 
 class ProductView extends React.Component {
   constructor(props) {
@@ -7,23 +8,29 @@ class ProductView extends React.Component {
 
   render () {
     return (
-      <div>
+      <div className={styles.pics}>
         <img
+        className={styles.topLeft}
         src={this.props.product.main_img}
         />
         <img
+        className={styles.topRight}
         src={this.props.product.front_view}
         />
         <img
+        className={styles.midLeft}
         src={this.props.product.back_view}
         />
         <img
+        className={styles.midRight}
         src={this.props.product.bottom_view}
         />
         <img
+        className={styles.botLeft}
         src={this.props.product.top_view}
         />
         <img
+        className={styles.botRight}
         src={this.props.product.right_side}
         />
       </div>
