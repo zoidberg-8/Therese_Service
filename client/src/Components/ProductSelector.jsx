817b2502487b500dc971648099dcb158125aa5db
Therese Brown <browntm96@gmail.com>
2019-04-13 00:45:36 -0700

    Create static images and add css module classes

--- client/src/Components/ProductSelector.jsx
+++ client/src/Components/ProductSelector.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import styles from './Styles.module.css';
-// import ProductSelected from './ProductSelected.jsx';
 
 class ProductSelector extends React.Component {
   constructor(props) {
@@ -10,7 +9,30 @@ class ProductSelector extends React.Component {
   render() {
     return (
     <div className={styles.selector}>
-      <img className={styles.selectImg} src={this.props.shoe.main_img}/>
+      {/* <img
+      className={styles.selectImg}
+      src={this.props.shoe.main_img}
+      /> */}
+      <img
+      className={styles.selectImg}
+      src='https://c.static-nike.com/a/images/t_PDP_144_v1/f_auto/dicle1kmn9lefvul4umj/air-vapormax-flyknit-3-mens-shoe-JsWsB7.jpg'
+      />
+      <img
+      className={styles.selectImg}
+      src='https://c.static-nike.com/a/images/t_PDP_144_v1/f_auto/voavdpvleqntejyunmx3/air-vapormax-flyknit-3-mens-shoe-JsWsB7.jpg'
+      />
+      <img
+      className={styles.selectImg}
+      src='https://c.static-nike.com/a/images/t_PDP_144_v1/f_auto/trrurwrgjoztj84v8vzc/air-vapormax-flyknit-3-mens-shoe-JsWsB7.jpg'
+      />
+      <img
+      className={styles.selectImg}
+      src='https://c.static-nike.com/a/images/t_PDP_144_v1/f_auto/htbt4rskmsrhxfhsoqs3/air-vapormax-flyknit-3-mens-shoe-JsWsB7.jpg'
+      />
+      <img
+      className={styles.selectImg}
+      src='https://c.static-nike.com/a/images/t_PDP_144_v1/f_auto/ytzdf6z0rhvcyf5yfpu2/air-vapormax-flyknit-3-mens-shoe-JsWsB7.jpg'
+      />
     </div>
     );
   }
