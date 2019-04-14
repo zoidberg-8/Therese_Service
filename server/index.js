7f179410790e40d9603ccc44bd85c56ec723c17c
hunterxdong <hunterdong123@gmail.com>
2019-04-13 17:50:48 -0700

    modified css to work better with proxy server

--- server/index.js
+++ server/index.js
@@ -1,39 +1,43 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const db = require('../database/index.js');
+const express = require("express");
+const bodyParser = require("body-parser");
+const db = require("../database/index.js");
 
 const app = express();
 const PORT = 7018;
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static(__dirname + '/../client/dist'));
+app.use("/shoes/:id", express.static(__dirname + "/../client/dist"));
 
-app.get('/initial', (req, res) => {
+app.use("/shoes/", express.static(__dirname + "/../client/dist"));
+
+app.use(express.static(__dirname + "/../client/dist"));
+
+app.get("/initial", (req, res) => {
   db.getInitial((err, results) => {
     if (err) {
-      console.error('Error for GET request for inital from server');
+      console.error("Error for GET request for inital from server");
     }
     res.json(results);
   });
 });
 
 //still working on
-app.get('/api/product/:id', (req, res) => {
+app.get("/api/product/:id", (req, res) => {
   let id = Number(req.params.id);
   db.getProduct(id, (err, results) => {
     if (err) {
-      console.log('Error for GET request for images from server');
+      console.log("Error for GET request for images from server");
     }
     res.json(results);
   });
 });
 
-app.get('/api/main', (req, res) => {
+app.get("/api/main", (req, res) => {
   db.getAllMains((err, results) => {
     if (err) {
-      console.log('Error for GET request for main images from server');
+      console.log("Error for GET request for main images from server");
     }
     res.json(results);
   });
